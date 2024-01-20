224a6bf3723b1efcfb747d4995c39fdb2d200eab
Akshay Anurag <16032523+AkshayAnurag@users.noreply.github.com>
2024-01-20 18:52:47 +0530

    Add initial draft of Uplabdhi Summary component

--- src/components/uplabdhiSummary.js
+++ src/components/uplabdhiSummary.js
@@ -1,40 +1,106 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
 import Chip from '@mui/material/Chip';
 import Grid from '@mui/material/Grid';
-import MailIcon from '@mui/icons-material/Mail';
-import Stack from '@mui/material/Stack';
-import ProfileImage from '../images/avatar.jpg';
 
 const UplabdhiSummary = () => {
+    const { content } = useSelector((state) => state.data);
+
+    const getTotalPublicProjects = () => {
+        return content.filter(
+            accomplishment => accomplishment.type === "project"
+        ).length
+    }
+
+    const getTotalIndustryCertificates = () => {
+        return content.filter(
+            accomplishment => accomplishment.type === "industry_certification"
+        ).length
+    }
+
+    const getTotalAssessmentCertificates = () => {
+        return content.filter(
+            accomplishment => accomplishment.type === "assessment_based_certification"
+        ).length
+    }
+
+    const getTotalCompletionCertificates= () => {
+        return content.filter(
+            accomplishment => accomplishment.type === "completion_certification"
+        ).length
+    }
+
+    const getTotalNonITCertificates = () => {
+        return content.filter(
+            accomplishment => accomplishment.type === "non_it_certification"
+        ).length
+    }
+
+    const getTotalLinkedProfiles = () => {
+        return content.filter(
+            accomplishment => accomplishment.type === "linked_profile"
+        ).length
+    }
+
+    const accomplishmentCounts = {
+        "Public Projects": {
+            count: getTotalPublicProjects(),
+            isClickable: true,
+            href: "/projects/all"
+        },
+        "Industry-recognized certificates": {
+            count: getTotalIndustryCertificates(),
+            isClickable: true,
+            href: "/certificates/industry/all"
+        },
+        "Assessment-based certificates": {
+            count: getTotalAssessmentCertificates(),
+            isClickable: true,
+            href: "/certificates/assessment/all"
+        },
+        "Course completion certificates": {
+            count: getTotalCompletionCertificates(),
+            isClickable: true,
+            href: "/certificates/completion/all"
+        },
+        "Non-IT certificates": {
+            count: getTotalNonITCertificates(),
+            isClickable: true,
+            href: "/certificates/non-it/all"
+        },
+        "Linked profiles": {
+            count: getTotalLinkedProfiles(),
+            isClickable: true,
+            href: "/profiles/all"
+        },
+    }
+
     return (
-        <Grid container spacing={1} columns={{ xs: 4, sm: 8, md: 12 }} direction="row"
-            justifyContent="flex-end"
-            alignItems="stretch">
-            <Grid item xs={2} sm={6} md={10}>
-                <Stack spacing={1} sx={{
-                    bgcolor: 'background.paper',
-                    boxShadow: 10,
-                    borderRadius: 2,
-                    p: 2
-                }}>
-                    <Chip label="Akshay Anurag" color="info" variant='outlined' />
-                    <Chip label="akshay.california@gmail.com" color="info" icon={<MailIcon color="info" />} variant='outlined' />
-                </Stack>
+        <Box sx={{
+            bgcolor: 'background.paper',
+            boxShadow: 10,
+            borderRadius: 2,
+            p: 2
+        }}>
+            <Grid container spacing={2} columns={{ xs: 4, sm: 8, md: 12 }}>
+                {
+                    Object.entries(accomplishmentCounts).map(
+                        ([accomplishmentName, accomplishmentDetails]) =>
+                            <Grid item xs={2} sm={4} md={4} key={accomplishmentName}>
+                                <Chip
+                                    avatar={<Avatar>{accomplishmentDetails["count"]}</Avatar>}
+                                    label={accomplishmentName}
+                                    clickable={accomplishmentDetails["href"] ? true : false}
+                                    component={accomplishmentDetails["href"] ? "a" :  null}
+                                    href={accomplishmentDetails["href"] ? accomplishmentDetails["href"] :  null}
+                                />
+                            </Grid>
+                    )
+                }
             </Grid>
-            <Grid item xs={2} sm={2} md={2}>
-                <Avatar
-                    alt="Akshay Anurag"
-                    src={ProfileImage}
-                    sx={{
-                        width: 100, height: 102,
-                        bgcolor: 'background.paper',
-                        boxShadow: 20,
-                        borderRadius: 2
-                    }}
-                />
-            </Grid>
-        </Grid>
+        </Box>
     );
 }
 
